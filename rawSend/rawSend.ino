76bc4a28bbc1bda05cfe7566b93050d92b5a8eab
Lindsey Bieda <hazardous.narayan@gmail.com>
2017-06-22 11:37:22 -0400

    Theoretically this should work, but it doesn't because my mic is getting no data

--- rawSend/rawSend.ino
+++ rawSend/rawSend.ino
@@ -2,15 +2,19 @@
 #include <IRLibSendBase.h>
 #include <IRLib_HashRaw.h>
 
+#define RAW_DATA_LEN 100
+#define MIC_PIN A0
+
 IRsendRaw mySender;
+int sample;
+const int threshold = 800;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("Every time you press a key is a serial monitor we will send."));
+  Serial.println(F("Starting..."));
 }
 
 // RAW volume up data
-#define RAW_DATA_LEN 100
 uint16_t rawData[RAW_DATA_LEN]={
   65506, 66, 65502, 78, 65502, 86, 65502, 90, 
   65502, 86, 65502, 86, 65506, 86, 65502, 86, 
@@ -28,11 +32,10 @@ uint16_t rawData[RAW_DATA_LEN]={
 
    
 void loop() {
-  if (Serial.read() != -1) {
-    //send a code every time a character is received from the 
-    // serial port. You could modify this sketch to send when you
-    // push a button connected to an digital input pin.
-    mySender.send(rawData,RAW_DATA_LEN,36);//Pass the buffer,length, optionally frequency
+  sample = analogRead(MIC_PIN);
+  if (sample > 0) { Serial.println(sample); }
+  if (sample > threshold) {
+    mySender.send(rawData,RAW_DATA_LEN,36);
     Serial.println(F("Sent signal."));
   }
 }
